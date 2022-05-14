4c0b7c4616c4c41cd30ab61f277d5f6e3a8d92f1
Sam <samjfom@gmail.com>
2022-05-14 21:06:33 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `samsite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
